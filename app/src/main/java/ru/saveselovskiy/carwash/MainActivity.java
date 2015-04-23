4b608730680306a274f2ab161800945ab816ffbd
SAVeselovskiy <veselovskiysergey94@gmail.com>
2015-04-23 21:56:31 +0400

    second commit

--- app/src/main/java/ru/saveselovskiy/carwash/MainActivity.java
+++ app/src/main/java/ru/saveselovskiy/carwash/MainActivity.java
@@ -1,9 +1,27 @@
 package ru.saveselovskiy.carwash;
 
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Fragment;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.Toast;
+
+import com.mikepenz.iconics.typeface.FontAwesome;
+import com.mikepenz.materialdrawer.Drawer;
+import com.mikepenz.materialdrawer.model.DividerDrawerItem;
+import com.mikepenz.materialdrawer.model.PrimaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SecondaryDrawerItem;
+import com.mikepenz.materialdrawer.model.SectionDrawerItem;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,8 +30,43 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        new Drawer()
+                .withActivity(this)
+                .withToolbar(toolbar)
+                .withActionBarDrawerToggle(true)
+                .withHeader(R.layout.drawer_header)
+                .addDrawerItems(
+                        new PrimaryDrawerItem().withName("Мои авто").withIdentifier(1),
+                        new DividerDrawerItem(),
+                        new PrimaryDrawerItem().withName("Автомойки"),
+                        new PrimaryDrawerItem().withName("История").withIcon(FontAwesome.Icon.faw_eye).withBadge("6").withIdentifier(3)).withOnDrawerListener(new Drawer.OnDrawerListener() {
+            @Override
+            public void onDrawerOpened(View drawerView) {
+                // Скрываем клавиатуру при открытии Navigation Drawer
+                InputMethodManager inputMethodManager = (InputMethodManager) MainActivity.this.getSystemService(Activity.INPUT_METHOD_SERVICE);
+                inputMethodManager.hideSoftInputFromWindow(MainActivity.this.getCurrentFocus().getWindowToken(), 0);
+            }
+
+            @Override
+            public void onDrawerClosed(View drawerView) {
+            }
+        })
+                .build();
     }
 
+//    public void onBackPressed() {
+//        // Закрываем Navigation Drawer по нажатию системной кнопки "Назад" если он открыт
+//        if (drawerResult.isDrawerOpen()) {
+//            drawerResult.closeDrawer();
+//        } else {
+//            super.onBackPressed();
+//        }
+//    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
