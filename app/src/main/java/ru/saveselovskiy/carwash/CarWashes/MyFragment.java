61d6e69f1fdee05d825bcc2af104261d0064b674
SAVeselovskiy <sergaves646@yandex.ru>
2015-05-05 13:07:45 +0300

    marker + login

--- app/src/main/java/ru/saveselovskiy/carwash/CarWashes/MyFragment.java
+++ app/src/main/java/ru/saveselovskiy/carwash/CarWashes/MyFragment.java
@@ -23,6 +23,10 @@ import com.google.android.gms.maps.model.LatLngBounds;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
+import java.util.ArrayList;
+
+import ru.saveselovskiy.carwash.CarWashModel.CarWashesStorage;
+import ru.saveselovskiy.carwash.Carwash.Carwash;
 import ru.saveselovskiy.carwash.R;
 
 /**
@@ -42,45 +46,7 @@ public class MyFragment extends Fragment{
     private int position;
 
 
-    LatLng[][] geopoint_buildings = {
-            {
-                    new LatLng(55.790843,49.121839), new LatLng(55.790918,49.119216), new LatLng(55.791714,49.119677),
-                    new LatLng(55.791535,49.120342), new LatLng(55.789713,49.120220), new LatLng(55.789576,49.121503),
-                    new LatLng(55.790305,49.119814), new LatLng(55.791436,49.119374), new LatLng(55.794076,49.114069),
-                    new LatLng(55.793938,49.113179), new LatLng(55.786625,49.126034), new LatLng(55.761985,49.149218),
-                    new LatLng(55.791835,49.117663), new LatLng(55.792129,49.122164), new LatLng(55.792456,49.122475),
-                    new LatLng(55.792634,49.119939), new LatLng(55.793326,49.143072), new LatLng(55.793370,49.143328),
-                    new LatLng(55.787961,49.112449), new LatLng(55.784347,49.116633), new LatLng(55.789950,49.108446),
-                    new LatLng(55.787454,49.110386), new LatLng(55.785283,49.118412), new LatLng(55.789598,49.158523),
-                    new LatLng(55.822101,49.098264), new LatLng(55.793459,49.120393), new LatLng(55.791571,49.122873),
-                    new LatLng(55.789934,49.121850), new LatLng(55.789928,49.120686)
-            },
-            {
-                    new LatLng(55.790867,49.121813), new LatLng(55.794284,49.113565), new LatLng(55.786513,49.126308),
-                    new LatLng(55.791859,49.117667), new LatLng(55.792468,49.122351), new LatLng(55.792640,49.119869),
-                    new LatLng(55.790139,49.165480), new LatLng(55.792043,49.126202), new LatLng(55.793858,49.143994),
-                    new LatLng(55.787971,49.112440), new LatLng(55.784356,49.116623), new LatLng(55.787453,49.110399)
-            },
-            {
-                    new LatLng(55.791197,49.124089), new LatLng(55.742509,49.122628), new LatLng(55.793849,49.143935),
-                    new LatLng(55.788928,49.108037)
-            },
-            {
-                    new LatLng(55.788399,49.164457), new LatLng(55.789187,49.165110), new LatLng(55.785532,49.163392),
-                    new LatLng(55.790151,49.165469), new LatLng(55.785439,49.170386), new LatLng(55.785840,49.163399),
-                    new LatLng(55.791145,49.126074), new LatLng(55.792040,49.126202), new LatLng(55.805240,49.189937),
-                    new LatLng(55.786404,49.128802)
-            },
-            {
-                    new LatLng(55.790855,49.121786),new LatLng(55.791185,49.124105)
-            },
-            {
-                    new LatLng(55.791568,49.122867)
-            },
-            {
-                    new LatLng(55.790843,49.121839)
-            }
-    };
+    ArrayList<LatLng> geopoint_buildings = new ArrayList<LatLng>();
     String[] name_buildings;
 
     String[] address_buildings;
@@ -119,6 +85,11 @@ public class MyFragment extends Fragment{
                 false);
         up = this.getActivity();
         builder = new LatLngBounds.Builder();
+        Carwash[] array = CarWashesStorage.getCarWashes();
+        for (int i = 0; i < array.length; i++) {
+            LatLng point = new LatLng(array[i].latitude, array[i].longitude);
+            geopoint_buildings.add(i,point);
+        }
         return rootView;
     }
 
@@ -158,21 +129,21 @@ public class MyFragment extends Fragment{
         public void onInfoWindowClick(Marker marker) {
             String id = "1";
             LatLng pos = marker.getPosition();
-            for (int j = 0; j < geopoint_buildings[position].length; j++)
-                if (pos.equals(geopoint_buildings[position][j])){
-                    id = id_buildings[j];
-                    break;
-                }
-            Intent myIntent = new Intent(up, AdvertisingIdClient.Info.class);
-            myIntent.putExtra("id","" + id);
-            startActivity(myIntent);
+            for (int j = 0; j < geopoint_buildings.size(); j++);
+//                if (pos.equals(geopoint_buildings)){
+//                    id = id_buildings[j];
+//                    break;
+//                }
+//            Intent myIntent = new Intent(up, AdvertisingIdClient.Info.class);
+//            myIntent.putExtra("id","" + id);
+//            startActivity(myIntent);
         }
     }
 
     public void loadInfo() {
         if (geopoint_buildings != null) {
-            for (int j = 0; j < geopoint_buildings[position].length; j++) {
-                showObject(geopoint_buildings[position][j]);
+            for (int j = 0; j < geopoint_buildings.size(); j++) {
+                showObject(geopoint_buildings.get(j));
                 count++;
             }
         }
---
+++ app/src/main/java/ru/saveselovskiy/carwash/Login/Login.java
@@ -0,0 +1,39 @@
+package ru.saveselovskiy.carwash.Login;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by Sergey on 05.05.2015.
+ */
+public class Login extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+}
